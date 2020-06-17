25a31472b5bacded8f48f750cc6c64114b338e81
handilaw <handi.jkt@gmail.com>
2020-06-17 14:53:46 +0700

    gtau

---
+++ warning/ass.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ass extends CI_Model {
+
+	
+
+}
+
+/* End of file ass.php */
+/* Location: .//C/Users/HandiLaw/Me/tess/warning/ass.php */
