f82cfa09a0f6d988a402fbf66b55bb5407c528b8
handilaw <handi.jkt@gmail.com>
2020-06-17 15:07:55 +0700

    tay

---
+++ sdds/ass.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Testmodel extends CI_Model {
+
+	
+
+}
+
+/* End of file testmodel.php */
+/* Location: .//C/Users/Syauqi/Desktop/tess/testmodel.php */
+?>
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file test.php */
+/* Location: .//C/Users/Syauqi/Desktop/tess/test.php */
+?>
