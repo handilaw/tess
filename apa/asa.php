940b5ce6be558b69d1b9c1f892748210c4283bca
Handiyana NH <handi.jkt@gmail.com>
2020-06-18 13:06:05 +0700

    tes2

---
+++ apa/asa.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Asa extends CI_Model {
+
+	
+
+}
+
+/* End of file asa.php */
+/* Location: .//C/Users/HandiLaw/Me/tess/apa/asa.php */
